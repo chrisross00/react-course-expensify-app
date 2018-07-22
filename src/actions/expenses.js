e498cca996e7e54e53432a83347d6b28453ff11d
chris_macbook <chrisross00@gmail.com>
2018-07-22 12:23:59 -0400

    removed spread operation from startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -50,7 +50,7 @@ export const editExpense = (id, updates) => ({
 export const startEditExpense = (id, updates) => {
   return (dispatch) => {
     return database.ref(`expenses/${id}`).update({
-      ...updates
+      updates
     }).then(() => {
       dispatch(editExpense(id, updates));
     });
